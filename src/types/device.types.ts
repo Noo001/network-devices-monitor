1318cb89d478bb83ae4f30751b292edabf852ff8
Andrey <noo_@bk.ru>
2026-05-13 23:06:12 +0300

    Тип, моки, алиас

---
+++ src/types/device.types.ts
@@ -0,0 +1,15 @@
+export type DeviceStatus = 'online' | 'offline' | 'warning';
+
+export type DeviceType = 'switch' | 'olt' | 'ont';
+
+export interface Device {
+    id: string
+    type: DeviceType
+    name: string
+    ip: string
+    status: DeviceStatus
+    lastSeen: Date
+    model: string
+    firmware: string
+    uptime: number 
+}
